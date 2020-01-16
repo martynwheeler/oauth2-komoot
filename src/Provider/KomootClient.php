a695e2aa9f55d42717ac7c3101c0a336b2a61e60
martynwheeler <martyn.wheeler@physics.org>
2020-01-16 20:13:40 +0000

    Create KomootClient.php

---
+++ src/Provider/KomootClient.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * OAuth2 Client Bundle
+ * Copyright (c) KnpUniversity <http://knpuniversity.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace MartynWheeler\OAuth2\Client\Provider;
+
+use KnpU\OAuth2ClientBundle\Client\OAuth2Client;
+use League\OAuth2\Client\Token\AccessToken;
+use MartynWheeler\OAuth2\Client\Provider\KomootResourceOwner;
+
+class KomootClient extends OAuth2Client
+{
+    /**
+     * @return KomootResourceOwner|\League\OAuth2\Client\Provider\ResourceOwnerInterface
+     */
+    public function fetchUserFromToken(AccessToken $accessToken)
+    {
+        return parent::fetchUserFromToken($accessToken);
+    }
+    /**
+     * @return KomootResourceOwner|\League\OAuth2\Client\Provider\ResourceOwnerInterface
+     */
+    public function fetchUser()
+    {
+        return parent::fetchUser();
+    }
+}
